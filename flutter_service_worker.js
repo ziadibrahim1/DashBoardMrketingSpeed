7c9592a6a69d416aefd86f3afe1e0c7d6a11c38b
ziadibahim1 <ziadibrahim545@gmail.com>
2025-08-12 22:28:30 +0300

    Built web
    Branch: dev
    Commit: 5cc9ba0bae92fac5c6cd33e025889d08511b064b
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a7c01f0e6d01ac99b2d1cc92d9b33744
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "04bfde01386ea85b7e67fff994e4a05d",
+"flutter_bootstrap.js": "ad1d13989748d258de5439a3105c4adc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
